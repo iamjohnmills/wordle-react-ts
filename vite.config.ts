7c01245ac5a5e3f0f54f735c0e9f9d138094b782
John <john@Johns-MacBook-Pro.local>
2022-08-23 18:37:08 -0500

    added screenshot

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://iamjohnmills.github.io/wordle-react-ts/',
+  server: {
+    port: 3000,
+    host: true,
+    hmr: {
+      port: 3001,
+      clientPort: 5001,
+    },
+  },
+  preview:{
+    port: 3000
+  }
+  // base: 'https://iamjohnmills.github.io/wordle-react-ts/',
 })
